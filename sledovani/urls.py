4e64e417374d6ff05c40b7d15d59f788f6ee6292
jnosgps <baxgtx@gmail.com>
2017-01-30 16:41:31 +0100

    rider detailsoprava

--- sledovani/urls.py
+++ sledovani/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.riders_list, name='riders_list'),
-	url(r'^rider/(?P<pk>[0-9]+)/$', views.rider_detail, name='rider_detail'),
+	url(r'^rider/(?P<pk>\d+)/$', views.rider_detail, name='rider_detail'),
 ]
