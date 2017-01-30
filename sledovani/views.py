b8c22ca067726153e185baeb4ca5f6d689cc3b95
jnosgps <baxgtx@gmail.com>
2017-01-30 14:37:35 +0100

    views.py chyba v syntaxi..

--- sledovani/views.py
+++ sledovani/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . import Rider
 
-def riders_list:
+def riders_list(request):
 	riders = Rider.objects.all()
 	return render(request, 'sledovani/riders_list.html', {})
