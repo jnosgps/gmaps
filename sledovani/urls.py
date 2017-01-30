80b9b5f462da11971056c419c4f57504ea69d7ad
jnosgps <baxgtx@gmail.com>
2017-01-30 15:46:44 +0100

    rider detail

--- sledovani/urls.py
+++ sledovani/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.riders_list, name='riders_list'),
+	url(r'^rider/(?P<pk>[0-9]+)/$', views.rider_detail, name='rider_detail'),
 ]
--- sledovani/views.py
+++ sledovani/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Rider
 
 def riders_list(request):
 	riders = Rider.objects.all().order_by('up')
 	return render(request, 'sledovani/riders_list.html', {
 		'riders': riders,
+	})
+
+def rider_detail(request, pk):
+	rider = get_object_or_404(Rider, pk=pk)
+	return render(request, 'sledovani/rider_detail.html', {
+		'rider': rider
 	})
