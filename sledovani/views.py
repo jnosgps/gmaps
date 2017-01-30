49bcc761be06c800f5133e5e62f1c1d185f225fa
jnosgps <baxgtx@gmail.com>
2017-01-30 14:55:21 +0100

    views.py....

--- sledovani/views.py
+++ sledovani/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Rider
 
 def riders_list(request):
-	riders = Rider.objects.all()
+	riders = Rider.objects.all().order_by('up')
 	return render(request, 'sledovani/riders_list.html', {})
