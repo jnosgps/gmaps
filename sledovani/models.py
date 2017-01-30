ff16a8d99c93e571f69752f343ce301f0590b1c7
jnosgps <baxgtx@gmail.com>
2017-01-30 15:01:53 +0100

    chybaa u datetime muze byt jen auto_now autonow_add a nebo default

--- sledovani/models.py
+++ sledovani/models.py
@@ -5,7 +5,7 @@ class Rider(models.Model):
 	name = models.CharField(max_length=32)
 	delka = models.CharField(max_length=10)
 	sirka = models.CharField(max_length=10)
-	up = models.DateTimeField(auto_now=True, default=timezone.now)
+	up = models.DateTimeField(auto_now=True)
 	
 	def update(self, dx, dy):
 		delka = dx
