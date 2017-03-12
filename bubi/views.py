2354492a9cfecb3f21276df332defedb98ee5ca2
jnosgps <baxgtx@gmail.com>
2017-03-12 23:58:02 +0100

    zmena na bubi-burger, prvni test

--- bubi/views.py
+++ bubi/views.py
@@ -22,5 +22,5 @@ def kategorie(request, kat):
 	return render(request, 'bubi/category.html', {'produkty': prods})
 
 def produkt(request, pk):
-	produkt = Produkt.objects.get_object_or_404(pk=pk)
+	produkt = get_object_or_404(Produkt.objects.get(pk=pk))
 	return render(request, 'bubi/detail.html', {'produkt': produkt})
